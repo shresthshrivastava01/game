e1ccb7e6f2abbc53125ef9ab33bcfd0744a56615
shresth shrivastava <shrivastavashresth85@gmail>
2026-01-21 23:56:35 +0530

    first commit

--- streamlitui.py
+++ streamlitui.py
@@ -1,37 +1,37 @@
-import streamlit as st
-import random
+# import streamlit as st
+# import random
 
-st.title("🎯 Number Guessing Game")
+# st.title("🎯 Number Guessing Game")
 
-# Initialize game state
-if "number" not in st.session_state:
-    st.session_state.number = random.randint(1, 100)
-    st.session_state.attempts = 0
-    st.session_state.game_over = False
+# # Initialize game state
+# if "number" not in st.session_state:
+#     st.session_state.number = random.randint(1, 100)
+#     st.session_state.attempts = 0
+#     st.session_state.game_over = False
 
-st.write("Guess a number between **1 and 100**")
+# st.write("Guess a number between **1 and 100**")
 
-guess = st.number_input(
-    "Enter your number",
-    min_value=1,
-    max_value=100,
-    step=1
-)
+# guess = st.number_input(
+#     "Enter your number",
+#     min_value=1,
+#     max_value=100,
+#     step=1
+# )
 
-if st.button("Submit Guess") and not st.session_state.game_over:
-    st.session_state.attempts += 1
+# if st.button("Submit Guess") and not st.session_state.game_over:
+#     st.session_state.attempts += 1
 
-    if guess < st.session_state.number:
-        st.warning("📉 Too low!")
-    elif guess > st.session_state.number:
-        st.warning("📈 Too high!")
-    else:
-        st.success("🎉 aditi won!")
-        st.info(f"Attempts: {st.session_state.attempts}")
-        st.session_state.game_over = True
+#     if guess < st.session_state.number:
+#         st.warning("📉 Too low!")
+#     elif guess > st.session_state.number:
+#         st.warning("📈 Too high!")
+#     else:
+#         st.success("🎉 aditi won!")
+#         st.info(f"Attempts: {st.session_state.attempts}")
+#         st.session_state.game_over = True
 
-# Restart button
-if st.button("🔄 Restart Game"):
-    st.session_state.number = random.randint(1, 100)
-    st.session_state.attempts = 0
-    st.session_state.game_over = False
+# # Restart button
+# if st.button("🔄 Restart Game"):
+#     st.session_state.number = random.randint(1, 100)
+#     st.session_state.attempts = 0
+#     st.session_state.game_over = False
